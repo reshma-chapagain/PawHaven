f84eeec907f0aa816adfaf0884ea5fdc99122a2d
AaryaPradhan-76 <npradhanaarya@gmail.com>
2026-06-10 14:02:44 +0545

    Add Navigation Bar & Footer.Add filter buttons .Add Pet Cards & Images.Use jQuery Integration.

--- js/gallery.js
+++ js/gallery.js
@@ -11,3 +11,10 @@ function filterPets(type) {
         }
     });
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
